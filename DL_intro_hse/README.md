7df5d00032b40d8187c10facddb7e8d2eb663b01
Miron <miron@Mirons-MacBook-Pro.local>
2022-09-18 21:32:54 -0700

    3 lecs and sems passed

---
+++ DL_intro_hse/README.md
@@ -0,0 +1,2 @@
+Ссылка на курс лекций:
+https://www.youtube.com/playlist?list=PLEwK9wdS5g0qa3PIhR6HBDJD_QnrfP8Ei 
--- DL_intro_hse/seminars/filter_viz.py
+++ DL_intro_hse/seminars/filter_viz.py
@@ -91,22 +91,20 @@ def plot_filters_multi_channel(t):
 
 
 def plot_filters(layer, single_channel = True, collated = False):
-  #checking whether the layer is convolution layer or not 
-  if isinstance(layer, nn.Conv2d):
-    #getting the weight tensor data
-    weight_tensor = layer.weight.data
+    #checking whether the layer is convolution layer or not 
+    if isinstance(layer, nn.Conv2d):
+        #getting the weight tensor data
+        weight_tensor = layer.weight.data
     
-    if single_channel:
-      if collated:
-        plot_filters_single_channel_big(weight_tensor)
-      else:
-        plot_filters_single_channel(weight_tensor)
-        
+        if single_channel:
+            if collated:
+                plot_filters_single_channel_big(weight_tensor)
+            else:
+                plot_filters_single_channel(weight_tensor)
+        else:
+            if weight_tensor.shape[1] == 3:
+                plot_filters_multi_channel(weight_tensor)
+            else:
+                print("Can only plot weights with three channels with single channel = False")
     else:
-      if weight_tensor.shape[1] == 3:
-        plot_filters_multi_channel(weight_tensor)
-      else:
-        print("Can only plot weights with three channels with single channel = False")
-        
-  else:
-    print("Can only visualize layers which are convolutional")
+        print("Can only visualize layers which are convolutional")
